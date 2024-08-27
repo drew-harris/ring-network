5b8ce997491f772371a9c700480d438bded329b6
Drew Harris <harrisd@smu.edu>
2024-08-27 00:50:58 -0500

    remove eslint comments

--- core/src/utils.ts
+++ core/src/utils.ts
@@ -3,12 +3,10 @@ import { type ZodSchema, type z } from "zod";
 
 export function fn<
   Arg1 extends ZodSchema,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   Callback extends (arg1: z.output<Arg1>) => any,
 >(arg1: Arg1, cb: Callback) {
   const result = function (input: z.input<typeof arg1>): ReturnType<Callback> {
     const parsed = arg1.parse(input);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return cb.apply(cb, [parsed]);
   };
   result.schema = arg1;
@@ -23,7 +21,6 @@ type Mutator<
 
 export const mutation = <
   Arg1 extends ZodSchema,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   Callback extends (tx: WriteTransaction, arg1: z.output<Arg1>) => any,
 >(
   arg1: Arg1,
@@ -31,7 +28,6 @@ export const mutation = <
 ) => {
   const result = function (tx: WriteTransaction, input: z.input<typeof arg1>) {
     const parsed = arg1.parse(input);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return cb.apply(cb, [tx, parsed]);
   };
 
@@ -40,13 +36,11 @@ export const mutation = <
 
 type Query<
   Arg1 extends ZodSchema,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   Callback extends (tx: ReadTransaction, arg1: z.output<Arg1>) => any,
 > = (tx: ReadTransaction, arg1: z.input<Arg1>) => ReturnType<Callback>;
 
 export const query = <
   Arg1 extends ZodSchema,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   Callback extends (tx: ReadTransaction, arg1: z.output<Arg1>) => any,
 >(
   arg1: Arg1,
@@ -54,7 +48,6 @@ export const query = <
 ) => {
   const result = function (tx: ReadTransaction, input: z.input<typeof arg1>) {
     const parsed = input.parse(input);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return cb.apply(cb, [tx, parsed]);
   };
 
@@ -67,7 +60,6 @@ export class Mutations<Mutators> {
   public register<
     Name extends string,
     Arg1 extends ZodSchema,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     Callback extends (tx: WriteTransaction, arg1: z.output<Arg1>) => any,
   >(
     name: Name,
