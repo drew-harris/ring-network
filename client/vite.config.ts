83b360beed6b6730c05593b244a3487d15c5eea3
Drew Harris <harrisd@smu.edu>
2024-08-28 23:32:51 -0500

    pre shad 2

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
   clearScreen: false,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
