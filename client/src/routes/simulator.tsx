f98a701e891e25d16554f416715f892e7f58d50f
Drew Harris <harrisd@smu.edu>
2024-08-30 22:27:19 -0500

    move simulator

--- client/src/routes/simulator.tsx
+++ client/src/routes/simulator.tsx
@@ -1,26 +1,10 @@
-import { NodeView } from "@/components/node-viewing/NodeView";
-import { RealtimeClientContext } from "@/main";
+import { Outlet } from "@tanstack/react-router";
 import { createFileRoute } from "@tanstack/react-router";
-import { Node } from "core/node";
-import { useContext } from "react";
-import { useSubscribe } from "replicache-react";
 
 export const Route = createFileRoute("/simulator")({
-  component: SimulatorPage,
-  async loader({ context }) {
-    return await context.replicache.query(Node.queries.getAllNodes);
-  },
+  component: SimulatorLayout,
 });
 
-function SimulatorPage() {
-  const r = useContext(RealtimeClientContext);
-  const loadedData = Route.useLoaderData();
-  const data = useSubscribe(r, Node.queries.getAllNodes, {
-    default: loadedData,
-  });
-  return (
-    <div>
-      <NodeView nodes={data} />
-    </div>
-  );
+function SimulatorLayout() {
+  return <Outlet />;
 }
--- client/src/routes/simulator/index.tsx
+++ client/src/routes/simulator/index.tsx
@@ -1,6 +1,27 @@
+import { NodeView } from "@/components/node-viewing/NodeView";
+import { RealtimeClientContext } from "@/main";
 import { createFileRoute } from "@tanstack/react-router";
+import { Node } from "core/node";
+import { useContext } from "react";
+import { useSubscribe } from "replicache-react";
 
 export const Route = createFileRoute("/simulator/")({
-  component: () => <div>Hello /simulator/!</div>,
+  component: SimulatorIndexPage,
+  async loader({ context }) {
+    return await context.replicache.query(Node.queries.getAllNodes);
+  },
 });
+
+function SimulatorIndexPage() {
+  const r = useContext(RealtimeClientContext);
+  const loadedData = Route.useLoaderData();
+  const data = useSubscribe(r, Node.queries.getAllNodes, {
+    default: loadedData,
+  });
+  return (
+    <div>
+      <NodeView nodes={data} />
+    </div>
+  );
+}
 
