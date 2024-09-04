e61bc682d35bd8954250690bbf2073539b37bac4
Drew Harris <harrisd@smu.edu>
2024-09-04 15:59:38 -0500

    new navbar

--- client/src/components/layout/navbar.tsx
+++ client/src/components/layout/navbar.tsx
@@ -1,7 +1,8 @@
 export const Navbar = () => {
   return (
-    <div className="flex bg-neutral-800 flex-col p-4 items-center justify-center">
-      <div>navbar here</div>
+    <div className="flex border-b border-b-neutral-700 bg-neutral-800 p-4 px-8 items-center justify-between">
+      <div>Ring Network Simulator</div>
+      <div className="rounded-full bg-neutral-500 w-8 h-8"></div>
     </div>
   );
 };
