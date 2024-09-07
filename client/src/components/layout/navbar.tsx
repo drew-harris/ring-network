ba5868722c4d84705e04e523b9e19dc4e39022aa
Drew Harris <harrisd@smu.edu>
2024-09-06 22:07:32 -0500

    navbar popover

--- client/src/components/layout/navbar.tsx
+++ client/src/components/layout/navbar.tsx
@@ -1,23 +1,43 @@
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Link } from "@tanstack/react-router";
+
 export const Navbar = () => {
   return (
     <div className="flex border-b border-b-neutral-700 bg-neutral-800 py-2 px-8 items-center justify-between">
       <div>Ring Network Simulator</div>
-      <div className="rounded-full grid place-items-center bg-neutral-500 w-8 h-8">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-4 h-4"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-          />
-        </svg>
-      </div>
+      <Popover>
+        <PopoverTrigger>
+          <div className="rounded-full grid place-items-center bg-neutral-500 w-8 h-8">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-4 h-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+              />
+            </svg>
+          </div>
+        </PopoverTrigger>
+        <PopoverContent className="w-40">
+          <div className="text-sm pb-2 text-center">harrisd@smu.edu</div>
+          <Link
+            to="/login"
+            className="text-sm block hover:bg-neutral-800 rounded-md py-1 font-semibold text-center w-full"
+          >
+            Log Out
+          </Link>
+        </PopoverContent>
+      </Popover>
     </div>
   );
 };
